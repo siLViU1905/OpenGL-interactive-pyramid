036a668983e8b6aa079f3102669ce7e0545c8835
silviu <168079557+siLViU1905@users.noreply.github.com>
2025-03-08 22:56:11 +0200

    Add files via upload

---
+++ other/init.cpp
@@ -0,0 +1,34 @@
+#include "init.h"
+
+bool init(GLFWwindow*& window, const char* name, int width, int height)
+{
+    if (!glfwInit())
+        return false;
+
+    window = glfwCreateWindow(width, height, name, NULL, NULL);
+    if (!window)
+        return false;
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    glewExperimental = true;
+
+    GLenum err = glewInit();
+    if (err != GLEW_OK)
+        return false;
+
+
+    glEnable(GL_DEPTH_TEST);  // Enable depth testing for 3D
+
+    return true;
+}
+
+void reshape(GLFWwindow* window, int w, int h)
+{
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0, (double)w / (double)h, 0.1, 200.0); 
+    glMatrixMode(GL_MODELVIEW);
+}
