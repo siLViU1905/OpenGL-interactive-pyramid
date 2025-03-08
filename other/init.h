d1658134546332b58bba39a8e875ab8d80dd5c85
silviu <168079557+siLViU1905@users.noreply.github.com>
2025-03-08 22:55:25 +0200

    Update init.h

--- other/init.h
+++ other/init.h
@@ -1,1 +1,13 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
+#ifndef INITH
+#define INITH
+
+bool init(GLFWwindow*& window, const char* name, int width, int height);
+
+void display();
+
+void reshape(GLFWwindow* window,int w,int h);
+
+#endif
